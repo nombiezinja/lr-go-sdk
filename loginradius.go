9465d3043a557b9aa568bacafd9443f4ebc47f02
Ti Zhang <tianyizhang1987@gmail.com>
2019-02-20 15:23:22 -0800

    Adds token to header upon initiation with token

--- loginradius.go
+++ loginradius.go
@@ -26,25 +26,31 @@ type Context struct {
 	Request *httprutils.Request
 }
 
-func NewLoginradius(cfg *Config) (*Loginradius, error) {
+func NewLoginradius(cfg *Config, optionalArgs ...map[string]string) (*Loginradius, error) {
 
 	if cfg.ApiKey == "" || cfg.ApiSecret == "" {
 		errMsg := "Must initialize Loginradius client with ApiKey and ApiSecret"
 		err := lrerror.New("IntializationError", errMsg, errors.New(errMsg))
 		return nil, err
 	}
+
 	ctx := Context{
 		Request: &httprutils.Request{},
 	}
-	copyCfgToCtx(cfg, &ctx)
+
+	// If an access token is passed on initiation, create Auth Bearer token header
+	for _, arg := range optionalArgs {
+		if arg["token"] != "" {
+			tokenHeader := "Bearer " + arg["token"]
+			ctx.Request.Headers = map[string]string{"Authorization": tokenHeader}
+		}
+	}
+
+	ctx.ApiKey = cfg.ApiKey
+	ctx.ApiSecret = cfg.ApiSecret
 
 	return &Loginradius{
 		Context: &ctx,
 		Domain:  domain,
 	}, nil
-}
-
-func copyCfgToCtx(cfg *Config, ctx *Context) {
-	ctx.ApiKey = cfg.ApiKey
-	ctx.ApiSecret = cfg.ApiSecret
 }
