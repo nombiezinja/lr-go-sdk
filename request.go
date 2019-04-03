9694270e37ceffdc2ae24072f0b7550222e70f08
Ti Zhang <tianyizhang1987@gmail.com>
2019-04-03 16:12:26 -0700

    Adds comments to request.go

--- request.go
+++ request.go
@@ -7,10 +7,9 @@ import (
 	"github.com/nombiezinja/lr-go-sdk/lrerror"
 )
 
-// NewGetRequest constructs the request for Auth api end points requiring
-// Access token in the header and ApiKey in query param
+// NewGetRequest takes a uri and query parameters, then constructs a GET request for LoginRadius API endpoints requiring access tokens
+// being passed in Authorization Bearer header
 func (lr Loginradius) NewGetReqWithToken(path string, queries ...map[string]string) (*httprutils.Request, error) {
-
 	if lr.Context.Token == "" {
 		errMsg := "Must initialize Loginradius with access token for this API call."
 		err := lrerror.New("MissingTokenErr", errMsg, errors.New(errMsg))
@@ -38,6 +37,7 @@ func (lr Loginradius) NewGetReqWithToken(path string, queries ...map[string]stri
 	return request, nil
 }
 
+// NewGetRequest takes a uri and query parameters, then constructs a GET request for a LoginRadius API endpoint
 func (lr Loginradius) NewGetReq(path string, queries ...map[string]string) *httprutils.Request {
 	request := &httprutils.Request{
 		Method:      httprutils.Get,
@@ -53,6 +53,7 @@ func (lr Loginradius) NewGetReq(path string, queries ...map[string]string) *http
 	return request
 }
 
+// NewPostReqWithToken takes a uri, body, and query parameters, then constructs the request for LoginRadius PUT API end points requiring access tokens being passed in Authorization Bearer header
 func (lr Loginradius) NewPostReqWithToken(path string, body interface{}, queries ...map[string]string) (*httprutils.Request, error) {
 
 	if lr.Context.Token == "" {
@@ -88,8 +89,8 @@ func (lr Loginradius) NewPostReqWithToken(path string, body interface{}, queries
 	return request, nil
 }
 
+// NewPostReq takes a uri, body, and optional queries to construct a POST request for a LoginRadius POST API endpoint
 func (lr Loginradius) NewPostReq(path string, body interface{}, queries ...map[string]string) (*httprutils.Request, error) {
-
 	encodedBody, error := httprutils.EncodeBody(body)
 	if error != nil {
 		return nil, error
@@ -112,12 +113,11 @@ func (lr Loginradius) NewPostReq(path string, body interface{}, queries ...map[s
 			request.QueryParams[k] = v
 		}
 	}
-
 	return request, nil
 }
 
+// NewPutReq takes a uri, body, and optional queries to construct a PUT request for a LoginRadius API endpoint
 func (lr Loginradius) NewPutReq(path string, body interface{}, queries ...map[string]string) (*httprutils.Request, error) {
-
 	encodedBody, error := httprutils.EncodeBody(body)
 	if error != nil {
 		return nil, error
@@ -143,8 +143,9 @@ func (lr Loginradius) NewPutReq(path string, body interface{}, queries ...map[st
 	return request, nil
 }
 
+// NewPutReqWithToken takes a uri and query parameters, then constructs a PUT request for LoginRadius API endpoints requiring access tokens
+// being passed in Authorization Bearer header
 func (lr Loginradius) NewPutReqWithToken(path string, body interface{}, queries ...map[string]string) (*httprutils.Request, error) {
-
 	if lr.Context.Token == "" {
 		errMsg := "Must initialize Loginradius with access token for this API call."
 		err := lrerror.New("MissingTokenErr", errMsg, errors.New(errMsg))
@@ -174,10 +175,10 @@ func (lr Loginradius) NewPutReqWithToken(path string, body interface{}, queries 
 			request.QueryParams[k] = v
 		}
 	}
-
 	return request, nil
 }
 
+// NewDeleteReq takes a uri, body, and optional queries to construct a DELETE request for a LoginRadius POST API endpoint
 func (lr Loginradius) NewDeleteReq(path string, body ...interface{}) *httprutils.Request {
 	if len(body) != 0 {
 		encoded, err := httprutils.EncodeBody(body[0])
@@ -199,8 +200,9 @@ func (lr Loginradius) NewDeleteReq(path string, body ...interface{}) *httprutils
 	}
 }
 
+// NewDeleteReqWithToken takes a uri and query parameters, then constructs a PUT request for LoginRadius API endpoints requiring access tokens
+// being passed in Authorization Bearer header
 func (lr Loginradius) NewDeleteReqWithToken(path string, body interface{}, queries ...map[string]string) (*httprutils.Request, error) {
-
 	if lr.Context.Token == "" {
 		errMsg := "Must initialize Loginradius with access token for this API call."
 		err := lrerror.New("MissingTokenErr", errMsg, errors.New(errMsg))
@@ -234,16 +236,16 @@ func (lr Loginradius) NewDeleteReqWithToken(path string, body interface{}, queri
 	return request, nil
 }
 
+// AddApiCredentialsToReqHeader removes the apiKey query parameter from a constructed request
+// and add LoginRadius app credentials in the request headers
 func (lr Loginradius) AddApiCredentialsToReqHeader(req *httprutils.Request) {
 	delete(req.QueryParams, "apiKey")
 	req.Headers["X-LoginRadius-ApiKey"] = lr.Context.ApiKey
 	req.Headers["X-LoginRadius-ApiSecret"] = lr.Context.ApiSecret
 }
 
-func (lr Loginradius) AddApiKeyToReqHeader(req *httprutils.Request) {
-	req.Headers["X-LoginRadius-ApiKey"] = lr.Context.ApiKey
-}
-
+// NormalizeApiKey normalizes the apikey parameter in queries for requests to be sent to
+// LoginRadius endpoints that only accept "apikey"
 func (lr Loginradius) NormalizeApiKey(req *httprutils.Request) {
 	delete(req.QueryParams, "apiKey")
 	req.QueryParams["apikey"] = lr.Context.ApiKey
